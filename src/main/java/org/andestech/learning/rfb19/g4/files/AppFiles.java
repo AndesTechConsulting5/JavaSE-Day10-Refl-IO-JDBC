3836e6bf49c356867398c1da494109d44edc3dfb
a0z9 <a0z9@rambler.ru>
2019-04-26 19:31:21 +0300

    JDBC

--- src/main/java/org/andestech/learning/rfb19/g4/files/AppFiles.java
+++ src/main/java/org/andestech/learning/rfb19/g4/files/AppFiles.java
@@ -1,7 +1,12 @@
 package org.andestech.learning.rfb19.g4.files;
 
+import org.andestech.learning.rfb19.g4.utils.Book;
+
 import java.io.*;
+import java.io.DataOutputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.andestech.learning.rfb19.g4.utils.Utils.echo;
 
@@ -63,13 +68,96 @@ public class AppFiles {
 
             //2
             byte[] barr = new byte[fis.available()];
-
             fis.read(barr);
-
             System.out.println(new String(barr));
 
         }
         catch (IOException ex){ex.printStackTrace();}
+
+
+        //-------------------
+
+        File file5 = new File("e:\\datas4\\datas.bin");
+
+        try( DataOutputStream dos = new DataOutputStream(new FileOutputStream(file5)))
+        {
+           int data1 = 1231231333;
+           long data2 = 9847981479479847L;
+           byte data3 = 116;
+
+           dos.writeLong(data2);
+           dos.writeByte(data3);
+           dos.writeInt(data1);
+        }
+        catch (IOException ex){ex.printStackTrace();}
+
+
+        try( DataInputStream dis = new DataInputStream(new FileInputStream(file5)))
+        {
+            int data1;
+            long data2;
+            byte data3;
+
+            data2 = dis.readLong();
+            data3 = dis.readByte();
+            data1 = dis.readInt();
+
+            System.out.println("Number: " + data1 + ", " +data2 +", " + data3);
+        }
+        catch (IOException ex){ex.printStackTrace();}
+
+
+      // random
+
+       File file6 = new File("e:\\datas4\\raf.txt");
+echo('~');
+       try( RandomAccessFile raf = new RandomAccessFile(file6,"rw"))
+       {
+
+         // raf.write("1234567\nABCDEF\nАБВГДЕЁ".getBytes());
+          byte[] barr = new byte[(int)raf.length()];
+          raf.read(barr);
+          System.out.println(new String(barr));
+          //-----------
+          echo('>');
+          raf.seek(0);
+          byte[] barr2 = new byte[10];
+          raf.read(barr2,0,10);
+          System.out.println(new String(barr2));
+
+          raf.write("CCCCCCD".getBytes());
+
+       }
+       catch (IOException ex){ex.printStackTrace();}
+
+// Object IO
+
+        List<Book> library = new ArrayList<>(10);
+        library.add(new Book("Белое безмолвие","Джек Лондон",1899));
+        library.add(new Book("Крейцерова соната","Лев Толстой",1904));
+        library.add(new Book("Аэлита","Алексей Толстой",1932));
+
+        File file7 = new File("e:\\datas4\\library.bin");
+
+        try( ObjectOutputStream dos = new ObjectOutputStream(new FileOutputStream(file7)))
+        {
+            dos.writeObject(library);
+        }
+        catch (IOException ex){ex.printStackTrace();}
+
+
+       // List<Book> library2 = new ArrayList<>();
+        List<Book> library2 = null;
+
+        try( ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file7)))
+        {
+          library2 = (List<Book>)ois.readObject();
+        }
+        catch (IOException | ClassNotFoundException ex){ex.printStackTrace();}
+
+        System.out.println(library2);
+
+
 
 
     }
---
+++ src/main/java/org/andestech/learning/rfb19/g4/jdbc/AppJdbc.java
@@ -0,0 +1,91 @@
+package org.andestech.learning.rfb19.g4.jdbc;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Random;
+
+public class AppJdbc {
+
+    public static void main(String[] args) throws ClassNotFoundException
+    {
+
+        Class.forName("org.postgresql.Driver");
+
+        String url = "jdbc:postgresql://213.199.139.156/rfb19";
+        Properties props = new Properties();
+        props.setProperty("user","user01");
+        props.setProperty("password","*****");
+        props.setProperty("ssl","false");
+
+        Connection conn = null;
+        Statement statement = null;
+        PreparedStatement pstate = null;
+
+        try {
+            conn = DriverManager.getConnection(url, props);
+
+            System.out.println(conn);
+            conn.setAutoCommit(true);
+
+            statement =
+                    conn.createStatement();
+
+            pstate = conn.prepareStatement("insert into table01(id,data) values(?, ?)");
+
+
+
+
+           // statement.execute("create table table01(id int, data varchar(20));");
+
+//            statement.executeUpdate("insert into table01(id,data) values(1, 'Hello!')");
+//            statement.executeUpdate("insert into table01(id,data) values(2, 'Привет!')");
+
+            statement.executeUpdate("update table01 set data='Howdoyoudo!' where id=1");
+
+
+            for(int j = 3; j<28; j++)
+            {
+                String datas =  "Data" + new Random().nextInt(200000);
+                pstate.setInt(1,j);
+                pstate.setString(2,datas);
+
+                pstate.execute();
+
+            }
+
+
+
+
+            ResultSet rset = statement.executeQuery("select * from table01");
+
+            while(rset.next()){
+
+                System.out.println( rset.getInt(1) + " : " +
+                        rset.getString(2));
+            }
+
+
+
+
+
+
+
+            statement.close();
+            pstate.close();
+            conn.close();
+        }
+        catch (SQLException ex){ex.printStackTrace();}
+        finally {
+            try {
+                if (statement != null) statement.close();
+                if (conn != null) conn.close();
+            } catch (SQLException ex) {ex.printStackTrace();}
+
+        }
+
+
+
+
+
+    }
+}
